1589e769d84962a143de720f0fe5155d2359ad86
Henrique Moreira <h@serrasqueiro.com>
2020-02-20 21:14:22 +0000

    Mostly pylint compliant.

--- cotagente/trisconta/stockstick/stewstick.py
+++ cotagente/trisconta/stockstick/stewstick.py
@@ -1,18 +1,38 @@
 """ stewstick -- basic stock files
 """
 
-from sys import stdout, stdin, stderr
-from zexcess import *
-from ztable.xdate import *
+# pylint: disable=unused-argument
 
 
-CO_version="1.00 51"
+from sys import stdout, stderr
+from zexcess import ZSheets, ZTable, num_to_column_letters
+from ztable.xdate import MsDate, MsTime
+
+CO_version = "1.00 52"
 
 
-#
-# main()
-#
-def main (me, outFile, errFile, inArgs):
+def run_main(my_path, args):
+    """
+    Main script/ caller.
+    :param my_path: the entire argv 0
+    :param args: arguments
+    :return: None (wrong arguments) or an error-code.
+    """
+    me = my_path.replace("\\", "/").split("/")[-1].replace(".py", "")
+    code = main(me, stdout, stderr, args)
+    return code
+
+
+def main(me, outFile, errFile, inArgs):
+    """
+    Main program
+    :param me:
+    :param outFile: output stream
+    :param errFile: error stream
+    :param inArgs: input args
+    :return:
+    """
+    code = None
     if me=="stewstick":   # stewstick.py
         code = stewstick_main( outFile, errFile, inArgs )
     return code
@@ -27,47 +47,42 @@ def stewstick_main (outFile, errFile, inArgs):
     args = inArgs
     columns = None
     headingNr = 0
-    if len( args )<=0:
+    if len(args)<=0:
         return None
-    cmd = args[ 0 ]
-    param = args[ 1: ]
+    cmd = args[0]
+    param = args[1:]
     # Defaults
     # Options
-    while len( param )>0 and param[ 0 ].startswith( "-" ):
-        any = False
-        if param[ 0 ].find( "-v" )==0:
-            any = True
-            n = param[ 0 ].count( "v" )
-            if n+1!=len( param[ 0 ] ):
+    while len( param ) > 0 and param[0].startswith("-"):
+        if param[0].find("-v") == 0:
+            n = param[0].count("v")
+            if n + 1 != len(param[0]):
                 return None
             verbose += n
-            del param[ 0 ]
+            del param[0]
             continue
-        if param[ 0 ]=="-c":
-            columns = param[ 1 ]
-            del param[ :2 ]
+        if param[0] == "-c":
+            columns = param[1]
+            del param[:2]
             continue
-        if param[ 0 ]=="-g": # heading
-            headingNr = int( param[ 1 ] )
-            del param[ :2 ]
+        if param[0] == "-g":  # heading
+            headingNr = int(param[1])
+            del param[:2]
             continue
         return None
-    debug = 0 if verbose<3 else 1
-    aOpts = (
-      "verbose", debug,
-      )
+    debug = 0 if verbose < 3 else 1
     opts = {"verbose": verbose,
             "debug": debug,
             "col": columns,
             "heading-number": headingNr,
             }
     # Run command
-    if cmd=="version":
+    if cmd == "version":
         print("stewstick", CO_version)
         return 0
-    if cmd=="test":
+    if cmd == "test":
         return 0
-    if cmd=="dump":
+    if cmd == "dump":
         name = param[ 0 ]
         del param[ 0 ]
         rules = ("open",)
@@ -75,49 +90,73 @@ def stewstick_main (outFile, errFile, inArgs):
     return code
 
 
-#
-# dump()
-#
 def dump (outFile, errFile, name, param, opts, rules):
-    assert type(opts)==dict
+    """
+    Dump xls file.
+    :param outFile: output stream
+    :param errFile: error stream
+    :param name: filename
+    :param param: parameters
+    :param opts: other options
+    :param rules: ToDo
+    :return:
+    """
+    assert isinstance(opts, dict)
+    code = 0
+    debug = opts["debug"]
     onlyOne = True
-    verbose = opts[ "verbose" ]
-    cols = work_column_defs( opts[ "col" ] )
-    headingNr = opts[ "heading-number" ]
     z = ZSheets(name, param)
     sheets, cont = z.contents()
     for pages in cont:
-        y = 0
-        rowNr = 0
-        tbl = ZTable( pages )
-        for entry in tbl.cont:
-            y += 1
-            if y<=headingNr: continue
-            rowNr += 1
-            cIdx = 0
-            for cell in entry:
-                cIdx += 1
-                cLetter = num_to_column_letters( cIdx )
-                s = cell
-                if cols and cIdx<len( cols ):
-                    d = cols[cIdx]
-                    if d=="date":
-                        mDate = MsDate(cell)
-                        if mDate.jDate:
-                            if verbose>0:
-                                s = "{}='{}'".format( mDate, cell )
-                            else:
-                                s = str( mDate )
-                    elif d=="time":
-                        mTime = MsTime(cell)
-                        if verbose>0:
-                            s = "{}='{}'".format( mTime, cell )
-                        else:
-                            s = str( mTime )
-                print("{}{}: {}".format( cLetter, y, s ))
-            print("...\n")
+        code = dump_table(outFile, errFile, pages, param, opts, rules, debug)
         if onlyOne:
             break
+    return code
+
+
+def dump_table(outFile, errFile, pages, param, opts, rules, debug=1):
+    """
+    Dump one table.
+    :param outFile: output stream
+    :param errFile:  error stream
+    :param pages: pages to show
+    :param param: any other parameter
+    :param opts: options
+    :param rules: Rules
+    :return: int, an error-code
+    """
+    cols = work_column_defs(opts["col"])
+    headingNr = opts["heading-number"]
+    y = 0
+    rowNr = 0
+    tbl = ZTable(pages)
+    for entry in tbl.cont:
+        y += 1
+        if y <= headingNr: continue
+        rowNr += 1
+        cIdx = 0
+        for cell in entry:
+            cIdx += 1
+            cLetter = num_to_column_letters(cIdx)
+            cell_name = "{}{}".format(cLetter, y)
+            s = cell
+            if cols and cIdx < len(cols):
+                d = cols[cIdx]
+                if d == "date":
+                    mDate = MsDate(cell)
+                    if mDate.jDate:
+                        if debug > 0:
+                            s = "{}='{}'".format(mDate, cell)
+                        else:
+                            s = str(mDate)
+                elif d == "time":
+                    mTime = MsTime(cell)
+                    if debug > 0:
+                        s = "{}='{}'".format(mTime, cell)
+                    else:
+                        s = str(mTime)
+            outFile.write("{}: {}\n".format(cell_name, y, s))
+        outFile.write("...\n\n")
     return 0
 
 
@@ -131,11 +170,10 @@ def work_column_defs (s):
 # Main script
 #
 if __name__ == "__main__":
-    from sys import exit, argv
-    me = argv[ 0 ].replace( "\\", "/" ).split( "/" )[ -1 ].replace( ".py", "" )
-    code = main( me, stdout, stderr, argv[ 1: ] )
-    # See also account.py under packages/processors/
-    if code is None:
+    import sys
+    from sys import argv
+    CODE = run_main(argv[0], argv[1:])
+    if CODE is None:
         print("""stewstick COMMAND [options]
 
 Commands are:
@@ -148,6 +186,6 @@ Commands are:
 Options are:
   -v      Verbose (or use -vv, -vvv for more verbose).
 """)
-        code = 0
-    assert type( code )==int and code<=255
-    exit( code )
+        CODE = 0
+    assert isinstance(CODE, int) and CODE<=255
+    sys.exit(CODE)
