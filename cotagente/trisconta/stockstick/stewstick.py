c70f5a13e1aeb860bf902da6b6c391ebb7d200e3
Henrique Moreira <h@serrasqueiro.com>
2020-02-22 11:33:43 +0000

    Basic textual dump, skel!

--- cotagente/trisconta/stockstick/stewstick.py
+++ cotagente/trisconta/stockstick/stewstick.py
@@ -6,7 +6,7 @@
 
 from sys import stdout, stderr
 from zexcess import ZSheets, ZTable, num_to_column_letters
-from ztable.xdate import MsDate, MsTime
+from zrules import ZRules, cell_string, work_column_defs, keys_from_str
 
 CO_VERSION = "1.00 53"
 
@@ -55,6 +55,7 @@ def stewstick_main(outFile, errFile, inArgs):
     cmd = args[0]
     param = args[1:]
     # Defaults
+    opt_rules = ""
     # Options
     while len(param) > 0 and param[0].startswith("-"):
         if param[0].find("-v") == 0:
@@ -72,6 +73,10 @@ def stewstick_main(outFile, errFile, inArgs):
             headingNr = int(param[1])
             del param[:2]
             continue
+        if param[0] == "-k":
+            opt_rules = param[1]
+            del param[:2]
+            continue
         return None
     debug = 0 if verbose < 3 else 1
     opts = {"verbose": verbose,
@@ -79,17 +84,24 @@ def stewstick_main(outFile, errFile, inArgs):
             "col": columns,
             "heading-number": headingNr,
             }
+    # Common parameters
+    rules = ZRules(keys_from_str(opt_rules))
     # Run command
     if cmd == "version":
         print("stewstick", CO_VERSION)
         return 0
     if cmd == "test":
+        if opt_rules:
+            rules.dump()
         return 0
-    if cmd == "dump":
+    elif cmd == "dump":
         name = param[0]
         del param[0]
-        rules = ("open",)
         code = dump(outFile, errFile, name, param, opts, rules)
+    elif cmd == "textual":
+        name = param[0]
+        del param[0]
+        code = dump_textual_table(outFile, errFile, name, param, opts, rules)
     return code
 
 
@@ -117,7 +129,7 @@ def dump(outFile, errFile, name, param, opts, rules):
     return code
 
 
-def dump_table(outFile, errFile, pages, param, opts, rules, debug=1):
+def dump_table(outFile, errFile, pages, param, opts, rules, debug=0):
     """
     Dump one table.
     :param outFile: output stream
@@ -147,19 +159,7 @@ def dump_table(outFile, errFile, pages, param, opts, rules, debug=1):
             s = cell
             if cols and cIdx < len(cols):
                 d = cols[cIdx]
-                if d == "date":
-                    mDate = MsDate(cell)
-                    if mDate.jDate:
-                        if debug > 0:
-                            s = "{}='{}'".format(mDate, cell)
-                        else:
-                            s = str(mDate)
-                elif d == "time":
-                    mTime = MsTime(cell)
-                    if debug > 0:
-                        s = "{}='{}'".format(mTime, cell)
-                    else:
-                        s = str(mTime)
+                s = cell_string(cell, d, "-", debug)
             outFile.write("{}: {}\n".format(cell_name, s))
             dumped += 1
         if dumped > 0:
@@ -167,14 +167,45 @@ def dump_table(outFile, errFile, pages, param, opts, rules, debug=1):
     return 0
 
 
-def work_column_defs(s):
+def dump_textual_table(outFile, errFile, name, param, opts, rules):
     """
-    Work on those columns, defined by user arguments.
+    Dump xcel to textual file.
+    :param outFile: output stream
+    :param errFile:  error stream
+    :param pages: pages to show
+    :param param: any other parameter
+    :param opts: options
+    :param rules: Rules
+    :return: int, an error-code
     """
-    if s is None:
-        return None
-    cols = ["."] + s.split(":")
-    return cols
+    cols = work_column_defs(opts["col"])
+    headingNr = opts["heading-number"]
+    assert isinstance(opts, dict)
+    code = 0
+    z = ZSheets(name, param)
+    _, cont = z.contents()
+    for pages in cont:
+        y = 0
+        rowNr = 0
+        tbl = ZTable(pages)
+        for entry in tbl.cont:
+            y += 1
+            if y <= headingNr:
+                continue
+            rowNr += 1
+            cIdx = 0
+            dumped = 0
+            row = []
+            for cell in entry:
+                cIdx += 1
+                s = cell
+                if cols and cIdx < len(cols):
+                    d = cols[cIdx]
+                    s = cell_string(cell, d, "-")
+                row.append(s)
+            if row != []:
+                outFile.write("{}\n".format(row))
+    return 0
 
 
 #
