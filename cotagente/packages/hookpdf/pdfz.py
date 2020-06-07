ab614aed97851a826d5b5f25d0c4a980b977d0ee
Henrique Moreira <h@serrasqueiro.com>
2020-06-07 13:07:58 +0100

    pylint, and finalized pdfz.py/ poco.py

--- cotagente/packages/hookpdf/pdfz.py
+++ cotagente/packages/hookpdf/pdfz.py
@@ -1,24 +1,37 @@
-# pdfz.py  (c)2019  Henrique Moreira (part of 'camelchassis')
+# pdfz.py  (c)2019, 2020  Henrique Moreira (part of 'hookpdf')
 
 """
-  pdfz - Extension to PyPDF2
+  Extension to PyPDF2
 
   Compatibility: python 3.
 """
 
-from PyPDF2.pdf import *
+# pylint: disable=invalid-name, unused-argument, missing-function-docstring
+
+import sys
+#import PyPDF2.pdf as updf
+from PyPDF2.pdf import \
+     ContentStream, \
+     TextStringObject, \
+     b_
 from PyPDF2.utils import u_
 
-#
-# test_pdfz()
-#
-def test_pdfz (outFile, errFile, inArgs):
-  code = None
-  return code
+
+def main():
+    """ Basic tests, if needed. """
+    args = sys.argv[1:]
+    code = test_pdfz(sys.stdout, sys.stderr, args)
+    assert isinstance(code, int)
+    sys.exit(code)
 
 
-def extractText (obj, whichOne="alt"):
-  def original_extractText (self):
+def test_pdfz (outFile, errFile, args):
+    """ Nothing currently! """
+    assert args == []
+    return 0
+
+
+def extractText (obj, which_one="alt"):
     """
     Locate all text drawing commands, in the order they are provided in the
     content stream, and extract the text.  This works well for some PDF
@@ -29,92 +42,86 @@ def extractText (obj, whichOne="alt"):
 
     :return: a unicode string object.
     """
-    text = u_("")
-    content = self["/Contents"].getObject()
-    if not isinstance(content, ContentStream):
-      content = ContentStream(content, self.pdf)
-    # Note: we check all strings are TextStringObjects.  ByteStringObjects
-    # are strings where the byte->string encoding was unknown, so adding
-    # them to the text here would be gibberish.
-    for operands, operator in content.operations:
-      if operator == b_("Tj"):
-        _text = operands[0]
-        if isinstance(_text, TextStringObject):
-          text += _text
-      elif operator == b_("T*"):
-        text += "\n"
-      elif operator == b_("'"):
-        text += "\n"
-        _text = operands[0]
-        if isinstance(_text, TextStringObject):
-          text += operands[0]
-      elif operator == b_('"'):
-        _text = operands[2]
-        if isinstance(_text, TextStringObject):
-          text += "\n"
-          text += _text
-      elif operator == b_("TJ"):
-        for i in operands[0]:
-          if isinstance(i, TextStringObject):
-            text += i
-        text += "\n"
+    def original_extractText (self):
+        text = u_("")
+        content = self["/Contents"].getObject()
+        if not isinstance(content, ContentStream):
+            content = ContentStream(content, self.pdf)
+        # Note: we check all strings are TextStringObjects.  ByteStringObjects
+        # are strings where the byte->string encoding was unknown, so adding
+        # them to the text here would be gibberish.
+        for operands, operator in content.operations:
+            if operator == b_("Tj"):
+                _text = operands[0]
+                if isinstance(_text, TextStringObject):
+                    text += _text
+            elif operator == b_("T*"):
+                text += "\n"
+            elif operator == b_("'"):
+                text += "\n"
+                _text = operands[0]
+                if isinstance(_text, TextStringObject):
+                    text += operands[0]
+            elif operator == b_('"'):
+                _text = operands[2]
+                if isinstance(_text, TextStringObject):
+                    text += "\n"
+                    text += _text
+            elif operator == b_("TJ"):
+                for i in operands[0]:
+                    if isinstance(i, TextStringObject):
+                        text += i
+                text += "\n"
+        return text
+
+
+    def alt_extractText (self):
+        """
+        Locate text and include "\n"
+
+        :return: a unicode string object.
+        """
+        pic = ""
+        tic = "~"
+        text = u_("")
+        content = self["/Contents"].getObject()
+        if not isinstance(content, ContentStream):
+            content = ContentStream(content, self.pdf)
+        for operands, operator in content.operations:
+            if operator == b_("Tj"):
+                _text = operands[0]
+                if isinstance(_text, TextStringObject):
+                    text += _text + pic
+            elif operator == b_("T*"):
+                text += "\n"
+            elif operator == b_("'"):
+                text += "\n"
+                _text = operands[0]
+                if isinstance(_text, TextStringObject):
+                    text += operands[0]
+            elif operator == b_('"'):
+                _text = operands[2]
+                if isinstance(_text, TextStringObject):
+                    text += "\n"
+                    text += _text
+            elif operator == b_("TJ"):
+                for i in operands[0]:
+                    if isinstance(i, TextStringObject):
+                        text += i
+                text += "\n"
+            else:
+                text += tic
+        return text
+
+
+    if which_one == "original":
+        text = original_extractText(obj)
+    elif which_one == "alt":
+        text = alt_extractText(obj)
+    else:
+        assert False
     return text
 
 
-  def alt_extractText (self):
-    """
-    Locate text and include "\n"
-
-    :return: a unicode string object.
-    """
-    pic = ""
-    tic = "~"
-    text = u_("")
-    content = self["/Contents"].getObject()
-    if not isinstance(content, ContentStream):
-      content = ContentStream(content, self.pdf)
-    for operands, operator in content.operations:
-      if operator == b_("Tj"):
-        _text = operands[0]
-        if isinstance(_text, TextStringObject):
-          text += _text + pic
-      elif operator == b_("T*"):
-        text += "\n"
-      elif operator == b_("'"):
-        text += "\n"
-        _text = operands[0]
-        if isinstance(_text, TextStringObject):
-          text += operands[0]
-      elif operator == b_('"'):
-        _text = operands[2]
-        if isinstance(_text, TextStringObject):
-          text += "\n"
-          text += _text
-      elif operator == b_("TJ"):
-        for i in operands[0]:
-          if isinstance(i, TextStringObject):
-            text += i
-        text += "\n"
-      else:
-        text += tic
-    return text
-
-
-  if whichOne=="original":
-    text = original_extractText( obj )
-  elif whichOne=="alt":
-    text = alt_extractText( obj )
-  else:
-    assert False
-  return text
-
-
-#
-# Test suite
-#
 if __name__ == "__main__":
-  import sys
-  args = sys.argv[ 1: ]
-  code = test_pdfz( sys.stdout, sys.stderr, args )
-  sys.exit( code )
-
+    main()
--- cotagente/packages/hookpdf/poco.py
+++ cotagente/packages/hookpdf/poco.py
@@ -1,205 +1,218 @@
-# poco.py  (c)2019  Henrique Moreira (inspired on 'jogomundo')
+# poco.py  (c)2019, 2020  Henrique Moreira
 
 """
   Dump pdf files in different ways.
-
-  Compatibility: python 2 and 3.
 """
 
+# pylint: disable=invalid-name, unused-argument, missing-function-docstring, no-self-use
 
+# Hints:
+#	see also basic: https://gist.github.com/serrasqueiro/62bf3ce8fcbd63433b75fb25b51addda
+
+import sys
 import PyPDF2
-from pdfz import extractText
-from redito import xCharMap
+from hookpdf.pdfz import extractText
+from waxpage.redit import char_map
 
 
-#
-# main_poco()
-#
-def main_poco (outFile, errFile, inArgs):
-  code = None
-  debug = 0
-  verbose = 0
-  outName = None
-  try:
-    cmd = inArgs[ 0 ]
-  except:
-    return None
-  param = inArgs[ 1: ]
-  # Parse args
-  while len( param )>0 and param[ 0 ].startswith( "-" ):
-    if param[ 0 ].startswith( "-v" ):
-      verbose += param[ 0 ].count( "v" )
-      del param[ 0 ]
-      continue
-    if param[ 0 ].startswith( "-o" ):
-      outName = param[ 1 ]
-      del param[ :2 ]
-      continue
-    print("Invalid option(s);\n", param)
-    return None
-  args = param
-  if verbose>=3:
-    print("Debug on!")
-    debug = 1
-  if outName is not None:
-    outFile = open( outName, "w" )
-  # Adjusts before run
-  readKind = "text"
-  if cmd=="seq-pdf":
-    readKind = "seq"	# no blanks
-  elif cmd=="textual-pdf":
-    readKind = "textual"
-  # Command run
-  if cmd in ("raw-pdf",
-             "textual-pdf",
-             "seq-pdf",
-             ):
-    inName = args[ 0 ]
-    tup = readerControl.pdf_text( inName, readKind, errFile, debug )
-    code = tup[ 0 ]
-    cont = tup[ 1 ]
-    if code!=0:
-      if code==2:
-        errFile.write("Cannot read pdf: {}\n".format( inName ))
-      else:
-        errFile.write("No content: {}\n".format(inName))
-    else:
-      for a in cont:
-        outFile.write("{}\n".format( a ))
-    code = 0
-  if outName is not None:
-    outFile.close()
-  return code
+def main():
+    code = main_poco(sys.stdout, sys.stderr, sys.argv[ 1: ])
+    if code is None:
+        print("""
+poco.py Command [options]
+
+Commands are:
+   raw-pdf
+       Show raw PDF chars.
+
+   textual-pdf
+       Show text PDF chars.
+
+   seq-pdf
+       Show PDF without excessive blanks
+""")
+        code = 0
+    assert isinstance(code, int)
+    assert code <= 127
+    sys.exit(code)
 
 
-#
-# CLASS ReaderControl
-#
+def main_poco (outFile, errFile, args):
+    code = None
+    debug = 0
+    verbose = 0
+    outName = None
+    if not args:
+        return None
+    cmd = args[0]
+    param = args[1:]
+    # Parse args
+    while param and param[ 0 ].startswith( "-" ):
+        if param[ 0 ].startswith( "-v" ):
+            verbose += param[ 0 ].count( "v" )
+            del param[ 0 ]
+            continue
+        if param[ 0 ].startswith( "-o" ):
+            outName = param[ 1 ]
+            del param[ :2 ]
+            continue
+        print("Invalid option(s);\n", param)
+        return None
+    if verbose >= 3:
+        print("Debug on!")
+        debug = 1
+    if outName is not None:
+        outFile = open(outName, "w")
+    # Adjusts before run
+    readKind = "text"
+    if cmd == "seq-pdf":
+        readKind = "seq"    # no blanks
+    elif cmd == "textual-pdf":
+        readKind = "textual"
+    # Command run
+    if cmd in ("raw-pdf",
+               "textual-pdf",
+               "seq-pdf",
+               ):
+        inName = param[0]
+        tup = readerControl.pdf_text( inName, readKind, errFile, debug )
+        code, cont = tup
+        if code:
+            if code == 2:
+                errFile.write("Cannot read pdf: {}\n".format( inName ))
+            else:
+                errFile.write("No content: {}\n".format(inName))
+        else:
+            for a in cont:
+                outFile.write("{}\n".format(a))
+        code = 0
+    if outName is not None:
+        outFile.close()
+    return code
+
+
 class ReaderControl:
-  def __init__ (self):
-    self.initFuncOriginal = PyPDF2.pdf.PageObject.extractText
-    self.excessiveStrsReplacement = (" ", ("\n", "\n(---)\n\n"))
-    self.excCount = 0
-    self.excMaxCount = 10**5
+    """ (PDF) reader control """
+    def __init__ (self):
+        self.initFuncOriginal = PyPDF2.pdf.PageObject.extractText
+        self.excessiveStrsReplacement = (" ", ("\n", "\n(---)\n\n"))
+        self.excCount = 0
+        self.excMaxCount = 10**5
 
 
-  #
-  # pdf_reader()
-  #
-  def pdf_reader (self, aFile):
-    fileReader = PyPDF2.PdfFileReader( aFile )
-    return fileReader
+    def pdf_reader (self, aFile):
+        """ Reader """
+        fileReader = PyPDF2.PdfFileReader(aFile)
+        return fileReader
 
 
-  #
-  # pdf_text()
-  #
-  def pdf_text (self, inName, readKind="text", errFile=None, debug=0):
-    assert type( inName )==str
-    res = []
-    isOriginal = readKind=="seq"
-    isTextual = readKind=="textual"
-    if isOriginal:
-      PyPDF2.pdf.PageObject.extractText = self.initFuncOriginal
-    else:
-      PyPDF2.pdf.PageObject.extractText = extractText
-    try:
-      op = open( inName, "rb" )
-    except:
-      op = None
-    if op is None:
-      return (2, res)
-    pReader = self.pdf_reader( op )
-    toASCII = "ASCII"
-    cont = self.raw_content( pReader, toASCII )
-    if cont is None:
-      op.close()
-      return (3, res)
-    idx = 0
-    for line in cont:
-      idx += 1
-      s = line.rstrip()
-      if isTextual:
-        readerControl.excCount = 0
-        s = trim_excessive( s, self.excessiveStrsReplacement )
-        if readerControl.excCount>=readerControl.excMaxCount:
-          if errFile is not None:
-            strShown = s.replace( "\n", "\\n" )
-            errFile.write("Excessive blanks ({}) in line {}, size {}: {:<.60}\n".format( readerControl.excCount, idx, len(s), strShown ))
-      if debug>0:
-        print("Debug: #{} (type: {}, len={}, strip_len={})".format( idx, type(line), len(line), len(s) ) )
-        print(s, "<<<")
-      res.append( s )
-    op.close()
-    return (0, res)
+    def pdf_text (self, inName, readKind="text", errFile=None, debug=0):
+        """ PDF text """
+        assert isinstance(inName, str)
+        res = []
+        isOriginal = readKind == "seq"
+        isTextual = readKind == "textual"
+        if isOriginal:
+            PyPDF2.pdf.PageObject.extractText = self.initFuncOriginal
+        else:
+            PyPDF2.pdf.PageObject.extractText = extractText
+        try:
+            op = open(inName, "rb")
+        except FileNotFoundError:
+            op = None
+        if op is None:
+            return (2, res)
+        pReader = self.pdf_reader(op)
+        toASCII = "ASCII"
+        cont = self.raw_content( pReader, toASCII )
+        if cont is None:
+            op.close()
+            return (3, res)
+        idx = 0
+        for line in cont:
+            idx += 1
+            s = line.rstrip()
+            if isTextual:
+                readerControl.excCount = 0
+                s = trim_excessive(s, self.excessiveStrsReplacement)
+                if readerControl.excCount >= readerControl.excMaxCount:
+                    if errFile is not None:
+                        strShown = s.replace( "\n", "\\n" )
+                        errFile.write("Excessive blanks ({}) in line {}, size {}: {:<.60}\n"
+                                      "".format( readerControl.excCount, idx, len(s), strShown ))
+            if debug > 0:
+                print("Debug: #{} (type: {}, len={}, strip_len={})"
+                      "".format( idx, type(line), len(line), len(s) ) )
+                print(s, "<<<")
+            res.append(s)
+        op.close()
+        return (0, res)
 
 
-  #
-  # raw_content()
-  #
-  def raw_content (self, pReader, altChr="?"):
-    toASCII = altChr=="ASCII"
-    nPages = pReader.numPages
-    res = []
-    for pageNum in range( nPages ):
-      tuv = pReader.getPage( pageNum ).extractText()
-      assert type(tuv)==str
-      pageCont = ""
-      last = ""
-      for c in tuv:
-        r = c
-        if ord( c )==0x2022:
-          r = "$ "
-        elif c=="~":
-          if last==c:
-            r = "\n"
-        elif ord( c )>=127:
-          if toASCII:
-            r = xCharMap.simpler_ascii( c )
-          elif altChr is not None:
-            r = altChr
-        pageCont += r
-        last = c
-      res.append( pageCont )
-    return res
+    def raw_content (self, pReader, altChr="?"):
+        """ Dump raw content """
+        toASCII = altChr == "ASCII"
+        nPages = pReader.numPages
+        res = []
+        for pageNum in range(nPages):
+            tuv = pReader.getPage(pageNum).extractText()
+            assert isinstance(tuv, str)
+            pageCont = ""
+            last = ""
+            for c in tuv:
+                r = c
+                if ord(c) == 0x2022:
+                    r = "(o) "
+                elif c == "~":
+                    if last == c:
+                        r = "\n"
+                elif ord(c) >= 127:
+                    if toASCII:
+                        r = simpler_ascii(c)
+                    elif altChr is not None:
+                        r = altChr
+                pageCont += r
+                last = c
+            res.append(pageCont)
+        return res
 
 
-#
-# trim_excessive()
-#
-def trim_excessive (s, chrs=(" ",), debug=1):
-  assert type( chrs )==tuple or type( chrs )==list
-  for tup in chrs:
-    sep = ""
-    if type( tup )==tuple:
-      e = tup[ 0 ]
-      sep = tup[ 1 ]
-      assert type( sep )==str
-    else:
-      assert type( tup )==str
-      e = tup
-    ee = e + e
-    if sep=="":
-      i = readerControl.excMaxCount
-      while i>0:
-        last = s
-        s = s.replace( ee, e )
-        if s==last:
-          break
-        readerControl.excCount += 1
-        i -= 1
-    else:
-      idx = 9999
-      while idx>=0:
-        idx -= 1
-        pos1 = s.find( ee )
-        pos2 = s.rfind( ee )
-        if pos1==-1 or pos1==pos2:
-          break
-        s = s.replace( ee, e )
-      if pos1!=-1:
-        s = s.replace( ee, sep )
-  return s
+def trim_excessive (s, chrs=(" ",), debug=0):
+    assert isinstance(chrs, (tuple, list))
+    for tup in chrs:
+        sep = ""
+        if isinstance(tup, tuple):
+            e, sep = tup
+            assert isinstance(sep, str)
+        else:
+            assert isinstance(tup, str)
+            e = tup
+        ee = e + e
+        if sep == "":
+            i = readerControl.excMaxCount
+            while i > 0:
+                last = s
+                s = s.replace(ee, e)
+                if s == last:
+                    break
+                readerControl.excCount += 1
+                i -= 1
+        else:
+            idx = 9999
+            while idx >= 0:
+                idx -= 1
+                pos1 = s.find(ee)
+                pos2 = s.rfind(ee)
+                if pos1 in (-1, pos2):
+                    break
+                s = s.replace(ee, e)
+            if pos1 != -1:
+                s = s.replace(ee, sep)
+    return s
+
+
+def simpler_ascii(a_chr):
+    return char_map.simpler_ascii(a_chr)
 
 
 #
@@ -207,21 +220,9 @@ def trim_excessive (s, chrs=(" ",), debug=1):
 #
 readerControl = ReaderControl()
 
+
 #
 # Main script
 #
 if __name__ == "__main__":
-  import sys
-  code = main_poco( sys.stdout, sys.stderr, sys.argv[ 1: ] )
-  if code is None:
-    print("""
-poco.py Command [options]
-
-Commands are:
-   raw-pdf file [file ...]
-       Show raw PDF chars.
-""")
-    code = 0
-  assert type( code )==int
-  assert code<=127
-  sys.exit( code )
+    main()
