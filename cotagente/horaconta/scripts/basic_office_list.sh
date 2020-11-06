7deec13e8063724482c4d992c7c60e86c895d3e9
Henrique Moreira <hmoreira@infinera.com>
2020-11-06 15:01:01 +0000

    Basic list from OL

--- cotagente/horaconta/scripts/basic_office_list.sh
+++ cotagente/horaconta/scripts/basic_office_list.sh
@@ -6,9 +6,20 @@
 
 basic_list ()
 {
- sed 's/Henrique Moreira/@~/g' | tr \\012 ' ' | tr @ \\012 | grep "PT "
+ sed 's/Henrique Moreira/@~/g' | tr \\012 ' ' | tr @ \\012 | grep " PT " | \
+	sed 's/ [P-Z][A-Z]* - Vacation Leave / vacation /' | \
+	sed 's/ PT - Home Office / WFO /;s/ more_horiz //;s/ \([A-Z]\)[A-Za-z]* info[ ]*$/ \1/' | \
+	sed 's/Unassigned=/UN/' | \
+	grep -v " Cancelled" | stripped
 }
 
+
+stripped ()
+{
+ sed 's/\([0-9][0-9-]*\) to \([0-9][0-9-]*\)/\1...\2/' | \
+	awk '{printf "%s, %s\n", $6, $0}' | \
+	sed 's/\.00//g'
+}
 
 
 #
